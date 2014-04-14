12d3567b9baf4acad81c29458662e071c965c035
engin tola <engintola@gmail.com>
2014-04-14 13:17:34 +0300

    pre-built sorting for float vectors

---
+++ include/kortex/sorting.h
@@ -0,0 +1,26 @@
+// ---------------------------------------------------------------------------
+//
+// This file is part of the <kortex> library suite
+//
+// Copyright (C) 2014 Engin Tola
+//
+// See LICENSE file for license information.
+//
+// author: Engin Tola
+// e-mail: engintola@gmail.com
+// web   : http://www.engintola.com
+//
+// ---------------------------------------------------------------------------
+#ifndef KORTEX_SORTING_H
+#define KORTEX_SORTING_H
+
+#include <vector>
+
+namespace kortex {
+
+    void sort_ascending ( std::vector<float>& arr);
+    void sort_descending( std::vector<float>& arr );
+
+}
+
+#endif
---
+++ src/sorting.cc
@@ -0,0 +1,30 @@
+// ---------------------------------------------------------------------------
+//
+// This file is part of the <kortex> library suite
+//
+// Copyright (C) 2014 Engin Tola
+//
+// See LICENSE file for license information.
+//
+// author: Engin Tola
+// e-mail: engintola@gmail.com
+// web   : http://www.engintola.com
+//
+// ---------------------------------------------------------------------------
+#include <algorithm>
+#include <kortex/sorting.h>
+
+using std::vector;
+
+namespace kortex {
+
+    inline bool float_cmp_l(const float& l, const float& r) { return l > r; }
+    inline bool float_cmp_s(const float& l, const float& r) { return l < r; }
+    void sort_ascending( vector<float>& arr) {
+        std::sort( arr.begin(), arr.end(), float_cmp_s );
+    }
+
+    void sort_descending( vector<float>& arr ) {
+        std::sort( arr.begin(), arr.end(), float_cmp_l );
+    }
+}
