809cb5d1fa0a9ed8efa6fccd99f65b0243682cbf
engin tola <engintola@gmail.com>
2014-11-10 18:06:37 +0200

    new min max routines

--- include/kortex/minmax.h
+++ include/kortex/minmax.h
@@ -25,6 +25,9 @@ namespace kortex {
     int min( const vector<float>& arr, float& min_val );
     int max( const vector<float>& arr, float& max_val );
 
+    int min( const vector<double>& arr, double& min_val );
+    int max( const vector<double>& arr, double& max_val );
+
     void find_2_minima( const float* cost, int np, int rad, int matches[2] );
 }
 
