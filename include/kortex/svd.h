b50b0b8b1a242d17feb88d99826d1edc640face2
engin tola <engintola@gmail.com>
2021-07-28 15:59:41 +0300

    svd update

--- include/kortex/svd.h
+++ include/kortex/svd.h
@@ -49,6 +49,14 @@ namespace kortex {
 
         void print() const;
 
+        double condition_number() const {
+            static const double tolerance = 1e-10;
+            double sd_max = m_Sd[    0];
+            double sd_min = m_Sd[m_d-1];
+            double cond = ( sd_min<tolerance ) ? sd_max/tolerance : sd_max/sd_min;
+            return cond;
+        }
+
     private:
         void init();
 
